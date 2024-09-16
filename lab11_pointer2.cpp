675a6158b6722ba74257f0737b9522b24bd60738
winstonjose01 <winston.jose@gmail.com>
2024-09-16 15:18:10 -0700

    revision - 1

--- lab11_pointer2.cpp
+++ lab11_pointer2.cpp
@@ -2,3 +2,27 @@
 using namespace std;
 
 
+struct BankCustomer{
+
+    string first_name;
+    string last_name;
+    int phone_no;
+    int social_security_no;
+    int * no_bank_accounts;
+    double * balance;
+    
+    ~BankCustomer() {
+        if (no_bank_accounts)
+            delete no_bank_accounts;
+        no_bank_accounts = nullptr;
+        if (balance)
+            delete balance;
+        balance = nullptr;
+
+    }
+};
+
+main(){
+
+    
+}
