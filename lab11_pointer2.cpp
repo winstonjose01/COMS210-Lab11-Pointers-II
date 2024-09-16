1bb419ab5c2287da97f82e16aaa23c6c134396c4
winstonjose01 <winston.jose@gmail.com>
2024-09-16 16:18:33 -0700

    revision - 2

--- lab11_pointer2.cpp
+++ lab11_pointer2.cpp
@@ -1,28 +1,61 @@
 #include <iostream>>
 using namespace std;
 
+const int NO_CUSTOMERS = 4;
 
 struct BankCustomer{
-
     string first_name;
     string last_name;
-    int phone_no;
+    int phone;
     int social_security_no;
-    int * no_bank_accounts;
+    int * bank_accounts;
     double * balance;
     
     ~BankCustomer() {
-        if (no_bank_accounts)
-            delete no_bank_accounts;
-        no_bank_accounts = nullptr;
+        if (bank_accounts)
+            delete bank_accounts;
+        bank_accounts = nullptr;
         if (balance)
             delete balance;
         balance = nullptr;
 
     }
 };
+// Function declaration
+void inputCustomerInfo (BankCustomer *);
+void printCustomerInfo (BankCustomer *);
 
 main(){
 
-    
+    BankCustomer *customers = new BankCustomer[NO_CUSTOMERS];
+
+    for (int i = 0; i < NO_CUSTOMERS; i++){
+        inputCustomerInfo (&customers[i]);
+    }
+
+    for (int i = 0; i < NO_CUSTOMERS; i++){
+        printCustomerInfo (&customers[i]);
+    }
+
+}
+
+void inputCustomerInfo (BankCustomer * custptr){
+    static int numCust = 1;
+    cout << "Enter the account details for Customer #" << numCust << ": \n";
+
+    cout << "First Name: ";
+    getline(cin, custptr->first_name);
+    cout << "Last Name: ";
+    getline(cin, custptr->last_name);
+    cout << "Phone No: ";
+    cin >> custptr -> phoe
+    cout 
+
+
+}
+
+void printCustomerInfor (BankCustomer *custptr){
+
+
+
 }
